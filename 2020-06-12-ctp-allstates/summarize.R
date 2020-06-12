465ab506c87bd1786a844ca3760c47583cf7735d
Marcus Russi <marcusrussi@gmail.com>
2020-06-12 11:04:20 -0400

    2020-06-12 run skeletons
    Former-commit-id: b2c26898d101a6f98c60f185a38e2947eec389b5

---
+++ 2020-06-12-ctp-allstates/summarize.R
@@ -0,0 +1,25 @@
+library(furrr)
+library(covidcast)
+plan(multicore) # enable multicore execution
+
+d <- readRDS('results.rds')
+
+# Map each for of `d`, row-bind the result, do it in parallel
+r <- purrr::pmap_dfr(
+  d,
+  function(..., result) {
+    message(glue::glue("Summarizing {item}",
+            item = paste(list(...), collapse='-')))
+
+    tibble::tibble(
+      ...,
+      # Need to `list` it to avoid binding problems
+      summary = list(summary(result$result, index=TRUE))
+    )
+  }
+)
+
+# Unnest the new summary tibble
+r_unnest <- tidyr::unnest(r, summary)
+
+readr::write_csv(r_unnest, 'summary.csv')
