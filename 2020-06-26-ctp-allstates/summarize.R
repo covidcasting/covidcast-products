8fa171b0964026a534c9690eda0a55a942595e9b
Marcus Russi <marcusrussi@gmail.com>
2020-06-26 12:57:16 -0400

    2020-06-26

---
+++ 2020-06-26-ctp-allstates/summarize.R
@@ -0,0 +1,27 @@
+library(furrr)
+library(covidestim)
+plan(multicore) # enable multicore execution
+
+covidcast_register() # For backwards-compatibility with S3 methods
+
+d <- readRDS('results.rds')
+
+# Map each for of `d`, row-bind the result, do it in parallel
+r <- purrr::pmap_dfr(
+  d,
+  function(..., result) {
+    message(glue::glue("Summarizing {item}",
+            item = paste(list(...), collapse='-')))
+
+    tibble::tibble(
+      ...,
+      # Need to `list` it to avoid binding problems
+      summary = list(summary(result$result, index=TRUE))
+    )
+  }
+)
+
+# Unnest the new summary tibble
+r_unnest <- tidyr::unnest(r, summary)
+
+readr::write_csv(r_unnest, 'summary.csv')
