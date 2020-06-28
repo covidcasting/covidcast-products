c8eeab5812653b5f2b1c4d02cd5e820525a551cf
Marcus Russi <marcusrussi@gmail.com>
2020-06-28 00:52:29 -0400

    Script to convert summary.csv to the JSON format used in Rt.Live (partially implemented)

---
+++ 2020-06-26-ctp-allstates/RtLiveConvert.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(usmap)
+library(jsonlite)
+
+d <- read_csv('summary.csv')
+
+# Split each state into its own group, then split each group into its own df
+d_split <- d %>% group_by(state) %>% group_split()
+
+# Get a list of the statenames in the split representation. These will then
+# be used to key this list of df's
+d_statenames <- map_chr(d_split, ~unique(.$state))
+
+# Key the list
+d_indexed <- d_split %>% setNames(d_statenames)
+
+# Remove unneeded information and transpose
+process_state <- function(df) {
+
+  c("date"           = "date",
+    "corr_cases_new" = "cases.fitted",
+    "corr_cases_raw" = "cases.fitted",
+    "onsets"         = "infections",
+    "r0"             = "Rt",
+    "r0_l80"         = "Rt.lo",
+    "r0_h80"         = "Rt.hi"
+  ) -> vars_to_keep
+
+  df <- select_at(df, vars_to_keep)
+  df <- setNames(df, names(vars_to_keep))
+  df <- mutate(df, date = format(date, '%Y-%m-%d'))
+
+  transpose(df)
+}
+
+state_abbrs <- state.abb
+names(state_abbrs) <- state.name
+state_abbrs = c(state_abbrs, "District of Columbia" = "DC")
+
+restructure_state <- function(lst, state_name) {
+  list(
+    identifier = state_abbrs[state_name],
+    series     = lst,
+    population = usmap::statepop[[which(statepop$full == state_name), 'pop_2015']]
+  )
+}
+
+d_transposed      <- map(d_indexed, process_state)
+d_withtags        <- imap(d_transposed, restructure_state)  
+names(d_withtags) <- state_abbrs[names(d_withtags)]
+
+list(
+  state_data = d_withtags,
+  last_updated_ts = 1e3*(max(d$date) %>% as.POSIXct %>% as.numeric),
+  last_r0_date = d$date[length(d$date) - 2] %>% format('%Y-%m-%d')
+) -> final
+
+print(toJSON(final, pretty = TRUE, auto_unbox = TRUE))
