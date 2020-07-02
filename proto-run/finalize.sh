dcd55b097345e537a3bf03ef73071c01ed332c7f
Marcus Russi <marcusrussi@gmail.com>
2020-07-02 16:01:20 -0400

    Autorunner now updates current_summary.pack file
    Former-commit-id: 7af11d3b512672565a5bde7fe2a5033d4348bef5

--- proto-run/finalize.sh
+++ proto-run/finalize.sh
@@ -15,8 +15,12 @@ Rscript RtLiveConvert.R # Convert summary.csv to summary.json
 # json2msgpack needs to be in $PATH. It can be easily installed from GitHub
 json2msgpack -bli summary.json -o summary.pack
 
+# copy the .pack file to the root directory, so that after merging into master
+# it will be the new .pack file that the website looks to for data
+cp summary.pack ../current_summary.pack
+
 git checkout draft || { echo "Checking out the 'draft' branch failed" >&2; exit 1; }
-git add summary.csv summary.pack
+git add data.csv summary.csv summary.pack ../current_summary.pack
 git commit -m "Summary files for $(basename $(dirname $(pwd)))" \
            --author "CovidEstimBot <marcusrussi+covidestim@gmail.com>" || \
   { echo "Commiting results failed" >&2; exit 1; }
