865e7356423631a3c035d3afdbcb630d6d56f219
Marcus Russi <mwr8@farnam1.farnam.hpc.yale.internal>
2020-07-04 13:45:14 -0400

    Removed docopt from summarize.R
    Former-commit-id: 050a2ca5e654eebad229d39e6d0764d20d5ea507

--- proto-run/summarize.R
+++ proto-run/summarize.R
@@ -2,32 +2,15 @@
 
 library(rslurm)
 
-library(docopt)
 library(readr)
 library(dplyr, warn.conflicts = FALSE)
 library(tidyr)
 library(assertthat)
 library(purrr, warn.conflicts = FALSE)
 library(glue,  warn.conflicts = FALSE)
-library(cli)
 library(stringr)
 
-glue('covidestim SLURM summarizer
-
-Usage:
-  {name} <sjob>
-  {name} (-h | --help)
-  {name} --version
-
-Options:
-  -h --help                 Show this screen.
-  --id-vars=<vars>          Grouping vars in <path>, separated by commas. [default: state]
-  --version                 Show version.
-', name = "summarize.R") -> doc
-
-args <- docopt(doc, version = 'covidestim SLURM summarizer 0.1')
-
-sjob    <- readRDS(args$sjob)
+sjob    <- readRDS('sjob.RDS')
 d       <- tibble::as_tibble(get_slurm_out(sjob, outtype = 'table'))
 
 result <- select_at(d, c('group_name', 'summary')) %>% unnest(summary)
