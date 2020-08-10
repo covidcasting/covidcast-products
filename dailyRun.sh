12151daab22b6b1d82e6001cd479fd0c2fdee476
Marcus Russi <mwr8@farnam1.farnam.hpc.yale.internal>
2020-08-09 23:05:04 -0400

    Upped time limit

--- dailyRun.sh
+++ dailyRun.sh
@@ -56,7 +56,7 @@ Rscript rslurm.R \
   --id-vars=state \
   --partition=covid \
   --summarize \
-  --time=240 \
+  --time=280 \
   --name=ctp_daily \
   data.csv || \
   { echo "Preparing rslurm setup failed" >&2; exit 1; }
