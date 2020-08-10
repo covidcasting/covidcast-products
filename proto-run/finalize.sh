9b1a77319ce3409da12ab27841b10c0f83180332
Marcus Russi <marcusrussi@gmail.com>
2020-08-09 22:28:09 -0400

    Update finalize.sh

--- proto-run/finalize.sh
+++ proto-run/finalize.sh
@@ -31,10 +31,6 @@ git push origin draft
 
 # Update RDS and SQLite files
 cd ..
-find . \ 
-  -maxdepth 1 \
-  -regex '.*2020-0[789]-[0-9][0-9]-allstates-ctp' \
-  -type d | \
-  xargs Rscript sqlLoad.R --file=summary.csv --sqlite=dailyRuns.db --rds=dailyRuns.RDS
+find . -maxdepth 1 -regex '\./2020-0[789]-[0-9][0-9]-allstates-ctp' -type d | xargs Rscript sqlLoad.R --file=summary.csv --sqlite=dailyRuns.db --rds=dailyRuns.RDS
 
 git checkout master
