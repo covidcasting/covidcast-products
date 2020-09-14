f503784b73f30530455a226d57a194fd021507e2
Marcus Russi <marcusrussi@gmail.com>
2020-09-14 13:25:02 -0400

    Upped timelimit to 10 hours

--- dailyRun.sh
+++ dailyRun.sh
@@ -56,7 +56,7 @@ Rscript rslurm.R \
   --id-vars=state \
   --partition=covid \
   --summarize \
-  --time=380 \
+  --time=600 \
   --name=ctp_daily \
   data.csv || \
   { echo "Preparing rslurm setup failed" >&2; exit 1; }
